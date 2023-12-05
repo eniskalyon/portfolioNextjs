d443b7f0a1e317e4d9555083ea5b429fc18c3895
Enis Kalyon <fzlnsklyn@gmail.com>
2023-12-05 21:32:49 +0000

    update projects motions

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import SectionDivider from "@/components/section-divider";
 import Intro from "../components/intro";
+import About from "@/components/about";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
    <Intro />
    <SectionDivider />
+   <About />
+   <Projects />
     </main>
   )
 }
