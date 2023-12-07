99d8fcdc39c2b7e057fb6ef23b4b73d1a4def5b2
Enis Kalyon <fzlnsklyn@gmail.com>
2023-12-07 18:53:43 +0000

    add clsx and react-intersection-observer, update context and states for the navbar tracking

--- components/project.tsx
+++ components/project.tsx
@@ -12,7 +12,8 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
         target: ref,
         offset: ["0 1", "1.33 1"],
     });
-    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
     return <>
     
@@ -20,12 +21,12 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
             ref={ref}
             style={{
                 scale: scaleProgress,
-                opacity: scrollYProgress,
+                opacity: opacityProgress,
             }}
-            className='mb-3 sm:mb-8 last:mb-0'
+            className='group mb-3 sm:mb-8 last:mb-0'
             >
                 <section
-                className='group relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem] even:pl-8 hover:bg-gray-200 transition'>
+                className='relative rounded-lg bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem] group-even:pl-8 hover:bg-gray-200 transition'>
                 <div className='group-even:ml-[18rem] pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full'>
                     <h3 className='text-2xl font-semibold'>{title}</h3>
                     <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
